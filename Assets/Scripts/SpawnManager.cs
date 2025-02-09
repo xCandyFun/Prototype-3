dd6f40749f99dfe069a7536a98984b7d164ac61b
Simon T. <simtho0624@gmail.com>
2025-02-09 14:01:53 +0100

    Add repeating obstacles spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(40,0,0); 
+    private Vector3 spawnPos = new Vector3(25,0,0); 
+    private float spawnDelay = 2.0f;
+    private float repeatRate = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", spawnDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle(){
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
