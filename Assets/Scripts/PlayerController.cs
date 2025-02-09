f242980a9c71edab420653163b87fe5b6126c7f9
Simon T. <simtho0624@gmail.com>
2025-02-09 13:27:14 +0100

    Add gravModi and change if

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        isOnGround = true;
     }
 }
