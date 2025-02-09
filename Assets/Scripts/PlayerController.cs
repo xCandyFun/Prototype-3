6fee3c31abc1dc680804b83e14084c4a9f4a5b45
Simon T. <simtho0624@gmail.com>
2025-02-09 13:06:06 +0100

    Start of prototype 3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+}
