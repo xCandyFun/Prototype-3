9f2c809b8d450810cea2d6c79026c2d4cbdbc0f8
Simon T. <simtho0624@gmail.com>
2025-02-11 15:08:26 +0100

    Add destroy obstacle

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
     private PlayerController playerControllerScript;
+    private float leftBound = -25.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +16,10 @@ public class MoveLeft : MonoBehaviour
     {
         if(playerControllerScript.gameOver == false){
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle")){
+            Destroy(gameObject);
         }
     }
 }
