78926cc5ef9d2856ff7c295a7888d5304339c350
Simon T. <simtho0624@gmail.com>
2025-02-11 13:56:36 +0100

    Make the repatetion smoother

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50){
+        if (transform.position.x < startPos.x - repeatWidth){
             transform.position = startPos;
         }
     }
