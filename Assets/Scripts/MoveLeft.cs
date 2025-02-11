7d3f264e98609a4b7e394343c467f2a245ca7ce7
Simon T. <simtho0624@gmail.com>
2025-02-11 14:58:32 +0100

    Add background and obstacle stop

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if(playerControllerScript.gameOver == false){
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(40,0.3f,-1.3f); 
+    private Vector3 spawnPos = new Vector3(30,0.3f,-1.3f); 
     private float spawnDelay = 2.0f;
     private float repeatRate = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
